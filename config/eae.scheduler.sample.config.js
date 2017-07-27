a03eb0a735a32f27d16fd17680fa63dd98f8582b
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-07-27 09:11:36 +0100

    first commit

---
+++ config/eae.scheduler.sample.config.js
@@ -0,0 +1,10 @@
+/* -------------------------------------------------------------------------------------------
+ *  Copyright (c) Axel oehmichen. All rights reserved.
+ *  Licensed under the Apache 2 License. See LICENSE in the project root for license information.
+ * ---------------------------------------------------------------------------------------- */
+
+module.exports = {
+    mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
+    port: 8080,
+	enableCors: true
+};
