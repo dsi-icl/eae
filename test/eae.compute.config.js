c46d05b97de13ced740d04935440a46704fe2b55
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-07-27 17:48:16 +0100

    remove headers and change license from MIT to apache 2

--- test/eae.compute.config.js
+++ test/eae.compute.config.js
@@ -1,7 +1,3 @@
-/* -------------------------------------------------------------------------------------------
- *  Copyright (c) Jean Grizet. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- * ---------------------------------------------------------------------------------------- */
 
 module.exports = {
     mongoURL: 'mongodb://db/eae',
