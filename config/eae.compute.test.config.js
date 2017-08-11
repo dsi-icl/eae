f3ff448126e295dab37379635c0c9358d100049a
Tezirg <jean.grizet16@imperial.ac.uk>
2017-08-11 14:27:13 +0100

    Update test config openstack swift

--- config/eae.compute.test.config.js
+++ config/eae.compute.test.config.js
@@ -2,7 +2,7 @@ module.exports = {
     mongoURL: 'mongodb://mongodb/eae',
     port: 80,
 	enableCors: true,
-    swiftURL: 'http://127.0.0.1:8080',
+    swiftURL: 'http://swift:8080',
     swiftUsername: 'test:tester',
     swiftPassword: 'testing'
 };
