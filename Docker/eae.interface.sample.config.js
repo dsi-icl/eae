26b12f8148071299ff912f03794564a6240dc18e
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-16 23:50:29 +0000

    first round of files. Started docker-compose but to be continued.

---
+++ Docker/eae.interface.sample.config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
+    port: 8080,
+	enableCors: true,
+    carriers: ['127.0.0.1:3001']
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# eae-deploy
-Contains deployment informations, docker-compose and deployment scripts for the eAE
+# eAE-deploy
+Contains deployment information, docker-compose and deployment scripts for the eAE
