1793cae7824e0f77f5f236a5fd3c258e8eb625f0
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-17 16:19:01 +0000

    a bit hasty push. Adding missing volumes for the containers

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ cp config/eae.compute.sample.config.js Docker/eae.compute.config.js
 cp config/eae.interface.sample.config.js Docker/eae.interface.config.js
 cp config/eae.scheduler.sample.config.js Docker/eae.scheduler.config.js
 
+cd Docker
+
 docker-compose build
 
 echo -e "To start the eAE please run 'docker-compose up'\n"
