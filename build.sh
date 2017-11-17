20cbe05c8359591b7bf2b556aa70207235db8151
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-17 15:56:00 +0000

    to be tested

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cp config/eae.carrier.sample.config.js Docker/eae.carrier.config.js
+cp config/eae.compute.sample.config.js Docker/eae.compute.config.js
+cp config/eae.interface.sample.config.js Docker/eae.interface.config.js
+cp config/eae.scheduler.sample.config.js Docker/eae.scheduler.config.js
+
+docker-compose build
+
+echo -e "To start the eAE please run 'docker-compose up'\n"
---
+++ config/eae.carrier.sample.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    mongoURL: 'mongodb://mongodb/eae',
+    port: 80,
+    enableCors: true,
+    swiftURL: 'http://swift:8080',
+    swiftUsername: 'test:tester',
+    swiftPassword: 'testing'
+};
