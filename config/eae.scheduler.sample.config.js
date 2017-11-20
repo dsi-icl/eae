3565c5519ede5fe9c503801a55885e3a4e02c73c
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-20 16:37:35 +0000

    wrong port for the scheduler.saml=ple config

--- config/eae.scheduler.sample.config.js
+++ config/eae.scheduler.sample.config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     mongoURL: 'mongodb://mongodb/eae',
-    port: 8080,
-	enableCors: true,
+    port: 80,
+    enableCors: true,
     jobsExpiredStatusTime: 720 , // Time in hours: 24h * 30d. Jobs to be archived
     jobsTimingoutTime: 24 , // Time in hours: 24h. Jobs to be cancelled for exceeding computing time policy.
     nodesExpiredStatusTime: 1, // Time in hours. Tolerance for the refresh of the nodes' status
