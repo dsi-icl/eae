4d677a61a4c06685cabc2037b7f5877dabf8129b
aoehmich <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-21 16:04:48 +0000

    make the messages a bit more readable

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ cd Docker
 
 docker-compose down
 
-echo -e "All instances have been deleted.\n To retrieve the latest images please run 'bash build.sh' otherwise please run 'bash start.sh'\n"
+echo -e "All instances have been deleted.\nTo retrieve the latest images please run 'bash build.sh' otherwise please run 'bash start.sh'\n"
