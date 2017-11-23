b52124b1c6c4c2381b574a1b3d9bf9c6ec33062b
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-23 23:19:15 +0000

    add a print to the python job to see the stdout in mongo log

--- test/files/test.py
+++ test/files/test.py
@@ -9,6 +9,7 @@ time.sleep(20)
 
 with open('output/test_out.txt', 'w') as f:
     f.write('Hello World !')
+    print('Hello World !')
     f.write(retrieve_params)
     with open('faust.txt', 'r') as f2:
         for i in range(0, 10):
