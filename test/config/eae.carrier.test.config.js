ef3224aeec038ada1ebc5c948f8d63280f9f6119
aoehmich <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-23 13:33:15 +0000

    adding the test configs to be edited later

---
+++ test/config/eae.carrier.test.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    mongoURL: 'mongodb://mongodb/eae',
+    port: 3000,
+    enableCors: true,
+    swiftURL: 'http://swift:8080',
+    swiftUsername: 'test:tester',
+    swiftPassword: 'testing'
+};
