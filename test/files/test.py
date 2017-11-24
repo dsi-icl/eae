2770a97a4211a9d4ba7586e3de90552b8694d2d3
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-11-24 00:45:07 +0000

    adding \n to make it slightly prettier

--- test/files/test.py
+++ test/files/test.py
@@ -2,13 +2,13 @@
 import sys
 import time
 
-retrieve_params = sys.argv[1]
+retrieve_params = sys.argv[1] + '\n'
 
 # We add a sleep to be able to test that the job is running
 time.sleep(20)
 
 with open('output/test_out.txt', 'w') as f:
-    f.write('Hello World !')
+    f.write('Hello World !\n')
     print('Hello World !')
     f.write(retrieve_params)
     with open('faust.txt', 'r') as f2:
