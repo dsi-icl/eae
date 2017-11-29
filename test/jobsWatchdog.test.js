34182dda0599e02a79692198204d268c301dfd16
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-11-29 01:05:09 +0000

    Test that SCHEDULED or RUNNING jobs which have timed out are queued again

--- test/jobsWatchdog.test.js
+++ test/jobsWatchdog.test.js
@@ -27,9 +27,42 @@ test('Expired completed jobs are archived', async () => {
     let jobs = await jobsWatchdogTestServer.mongo_helper.retrieveJobs({});
     let archived_jobs = await jobsWatchdogTestServer.mongo_helper.retrieveArchivedJobs({});
 
-    console.log(jobs);
-    console.log(archived_jobs);
-
     expect(jobs.length).toBe(0);
     expect(archived_jobs[0]._id).toEqual(job._id);
+});
+
+test('Running jobs which have timed out are queued again', async () => {
+    let job = {
+        status: [
+            Constants.EAE_JOB_STATUS_RUNNING
+        ],
+        executorIP: 'compute',
+        executorPort: 80,
+        startDate: new Date(Date.now() - 8640000), // Yesterday date
+        statusLock: false,
+    };
+
+    await jobsWatchdogTestServer.insertJob(job);
+    await jobsWatchdogTestServer.jobsWatchdog._invalidateTimingOutJobs();
+    let jobs = await jobsWatchdogTestServer.mongo_helper.retrieveJobs({});
+
+    expect(jobs[0].status).toEqual([Constants.EAE_JOB_STATUS_QUEUED, Constants.EAE_JOB_STATUS_RUNNING]);
+});
+
+test('Scheduled jobs which have timed out are queued again', async () => {
+    let job = {
+        status: [
+            Constants.EAE_JOB_STATUS_SCHEDULED
+        ],
+        executorIP: 'compute',
+        executorPort: 80,
+        startDate: new Date(Date.now() - 8640000), // Yesterday date
+        statusLock: false,
+    };
+
+    await jobsWatchdogTestServer.insertJob(job);
+    await jobsWatchdogTestServer.jobsWatchdog._invalidateTimingOutJobs();
+    let jobs = await jobsWatchdogTestServer.mongo_helper.retrieveJobs({});
+
+    expect(jobs[0].status).toEqual([Constants.EAE_JOB_STATUS_QUEUED, Constants.EAE_JOB_STATUS_SCHEDULED]);
 });
