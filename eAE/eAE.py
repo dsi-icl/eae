9c5b6901c41217020f92f9bacebc43b4c397944a
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-12-17 04:44:49 +0100

    first batch of implementations. Job creation and result retrieval to be continued. TO BE TESTED

--- eAE/eAE.py
+++ eAE/eAE.py
@@ -31,71 +31,124 @@ class eAE(object):
         return "\rThe interface ip is set to: {0}\r The interface port is set to: {1}".format(self.interface_ip,
                                                                                               self.interface_port)
 
-    def submit_jobs(self, parameters_set, cluster, computation_type, main_file, data_files, host_ip, ssh_port="22"):
+    def submit_jobs(self, computation_type, main_file, parameters_set, data_files):
         """
         Submit jobs to the eAE backend
 
         This method is called when a specific task needs to be computed on a cluster.
         """
 
-        configs = parameters_set
-        data = {'host_ip': host_ip, 'configs': configs,
-                'cluster': cluster, 'clusterType': computation_type, 'mainScriptExport': main_file}
-        data_str = json.dumps(data)
-        self.connection.request('POST', '/interfaceEAE/OpenLava/submitJob', data_str)
-        res = self.connection.getresponse()
-        submit_response = res.read()
+        if len(data_files) == len(parameters_set):
+            return "There is a mismatch in the number of data files data files and parameters " \
+                   + str(len(data_files)) + " , " + str(len(parameters_set))
 
-        return submit_response
+        submit_responses = []
+        for i in range(len(data_files)):
+            job = {"type": computation_type, "main": main_file, "params": parameters_set[i], "input": data_files[i] }
+            data = {'eaeUsername': self.username, 'eaeUserToken': self.password, 'job': job}
+            data_str = json.dumps(data)
+            self.connection.request('POST', '/job', data_str)
+            res = self.connection.getresponse()
+            submit_responses.append(res.read())
+
+        return submit_responses
 
     def get_job(self):
         """
         Retrieves a job currently running on the eAE backend
         """
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/servicesStatus', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-        return
+        return submit_response
+
 
     def get_all_jobs(self):
         """
         Retrieves all job currently running on the eAE backend
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/job/getAll', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-    def cancel_job(self):
+        return submit_response
+
+    def cancel_job(self, job_id):
         """
         Cancels a specific job currently running on the eAE backend
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password, "jobID": job_id}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/job/cancel', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-    def get_job_result(self):
+        return submit_response
+
+    def get_job_result(self, job_id):
         """
         Retrieves the results for a specific job computed on the eAE backend
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password, "jobID": job_id}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/job/results', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-    def get_user(self):
+        return submit_response
+
+    def get_user(self, requestedUsername):
         """
         Retrieves all the information regarding a specified user
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password, "requestedUsername": requestedUsername}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/user', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-    def create_user(self):
+        return submit_response
+
+    def create_user(self, new_user):
         """
         Creates a new user for the eAE
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password, "newUser": {"username": new_user}}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/user/create', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
 
-    def delete_user(self):
+        return submit_response
+
+    def delete_user(self, user_to_be_deleted):
         """
         Deletes the specified eAE user
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password, "userToBeDeleted": user_to_be_deleted}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/user/delete', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
+
+        return submit_response
 
     def get_services_status(self):
         """
         Retrieves all the current statuses of the eAE backend services
         """
-        return
+        data = {'eaeUsername': self.username, 'eaeUserToken': self.password}
+        data_str = json.dumps(data)
+        self.connection.request('POST', '/servicesStatus', data_str)
+        res = self.connection.getresponse()
+        submit_response = res.read()
+
+        return submit_response
 
     def who_are_you(self):
         """
