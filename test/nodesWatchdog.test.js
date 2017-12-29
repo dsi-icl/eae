6de9f608312e05962bb9c7be1ddebcd44f386e88
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-12-29 18:05:14 +0100

    Test nodes that have been busy for long are invalidated

--- test/nodesWatchdog.test.js
+++ test/nodesWatchdog.test.js
@@ -1,7 +1,7 @@
 const { Constants } =  require('eae-utils');
 let NodesWatchdogTestServer = require('./nodesWatchdogTestServer.js');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 let nodesWatchdogTestServer = new NodesWatchdogTestServer();
 
@@ -26,4 +26,21 @@ test('Dead nodes are invalidated by setting their locks to true', async () => {
 
     expect(nodes.length).toBe(1);
     expect(nodes[0].statusLock).toEqual(true);
+});
+
+test('Nodes that have been busy for longer than a defined threshold are invalidated', async () => {
+    let node = {
+        status: Constants.EAE_SERVICE_STATUS_BUSY,
+        lastUpdate: new Date(Date.now() - 8640000), // Yesterday date
+        statusLock: false
+    };
+
+    await nodesWatchdogTestServer.insertNode(node);
+    await nodesWatchdogTestServer.nodesWatchdog._purgeExpired();
+
+    let nodes = await nodesWatchdogTestServer.mongo_helper.retrieveNodesStatus({_id: node._id});
+
+    expect(nodes.length).toBe(1);
+    expect(nodes[0].statusLock).toEqual(true);
+    expect(nodes[0].status).toEqual(Constants.EAE_SERVICE_STATUS_DEAD);
 });
