5e8bf515df9278cbd1ebbe8744e5b44333a15fd6
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-12-29 18:12:52 +0100

    Test nodes that have been locked for long are invalidated

--- test/nodesWatchdog.test.js
+++ test/nodesWatchdog.test.js
@@ -44,3 +44,20 @@ test('Nodes that have been busy for longer than a defined threshold are invalida
     expect(nodes[0].statusLock).toEqual(true);
     expect(nodes[0].status).toEqual(Constants.EAE_SERVICE_STATUS_DEAD);
 });
+
+test('Nodes that have been locked for longer than a defined threshold are invalidated', async () => {
+    let node = {
+        status: Constants.EAE_SERVICE_STATUS_LOCKED,
+        lastUpdate: new Date(Date.now() - 8640000), // Yesterday date
+        statusLock: false
+    };
+
+    await nodesWatchdogTestServer.insertNode(node);
+    await nodesWatchdogTestServer.nodesWatchdog._purgeExpired();
+
+    let nodes = await nodesWatchdogTestServer.mongo_helper.retrieveNodesStatus({_id: node._id});
+
+    expect(nodes.length).toBe(1);
+    expect(nodes[0].statusLock).toEqual(true);
+    expect(nodes[0].status).toEqual(Constants.EAE_SERVICE_STATUS_DEAD);
+});
