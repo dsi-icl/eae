d9a7f84340b63451fdb213fde3266c622baab96e
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-06-08 17:52:45 +0100

    to be tested for swift

--- src/jobExecutorR.js
+++ src/jobExecutorR.js
@@ -47,49 +47,83 @@ JobExecutorR.prototype._preExecution = function() {
     let _this = this;
 
     return new Promise(function (resolve, reject) {
-        // Compute input container name
-        let container_name = _this._jobID.toString() + '_input';
         // Create input directory
         fs.mkdirSync(path.join(_this._tmpDirectory, 'input'));
         let file_transfer_promises = [];
+        // We need to check what type of job we are dealing with swift or standard
+        if(!(_this._model.swiftData === null || _this._model.swiftData === undefined)){
+            let containers = [];
+            for(let k in _this._model.swiftData) containers.push(k);
+            containers.forEach(function (container_id) {
+                // Download each input file in the model from the swift store
+                _this._model.swiftData[container_id].forEach(function (file) {
+                    // Store the file in the input subdirectory
+                    let tmpDestination = path.join(_this._tmpDirectory, 'input', file);
 
-        // Download each input file in the model from the swift store
-        _this._model.input.forEach(function(file) {
-            // Store the file in the input subdirectory
-            let tmpDestination = path.join(_this._tmpDirectory, 'input', file);
+                    // Get download stream
+                    let p = _this._swift.getFileReadStream(container_id, file).then(function (rs) {
+                        let ws = fs.createWriteStream(tmpDestination); // Open file descriptor
+                        rs.pipe(ws); //Pipe received data to be written in the file
 
-            // Get download stream
-            let p = _this._swift.getFileReadStream(container_name, file).then(function(rs) {
-                let ws = fs.createWriteStream(tmpDestination); // Open file descriptor
-                rs.pipe(ws); //Pipe received data to be written in the file
-
-                // Listen to then end of the file transfer
-                resolve(new Promise(function(file_resolve, file_reject) {
-                    rs.on('end', function() {
-                        file_resolve(true);
+                        // Listen to then end of the file transfer
+                        resolve(new Promise(function (file_resolve, file_reject) {
+                            rs.on('end', function () {
+                                file_resolve(true);
+                            });
+                            rs.on('error', function (error) {
+                                file_reject(ErrorHelper('Downloading ' + file + ' failed', error));
+                            });
+                        }));
+                    }, function (error) {
+                        reject(ErrorHelper('Downloading ' + file + ' has error', error));
                     });
-                    rs.on('error', function(error) {
-                        file_reject(ErrorHelper('Downloading ' + file + ' failed', error));
-                    });
-                }));
-            }, function(error) {
-                reject(ErrorHelper('Downloading ' + file + ' has error', error));
+
+                    // Push the file transfer promise into array
+                    file_transfer_promises.push(p);
+                });
+            });
+        }else {
+            // Compute input container name
+            let container_name = _this._jobID.toString() + '_input';
+
+            // Download each input file in the model from the swift store
+            _this._model.input.forEach(function (file) {
+                // Store the file in the input subdirectory
+                let tmpDestination = path.join(_this._tmpDirectory, 'input', file);
+
+                // Get download stream
+                let p = _this._swift.getFileReadStream(container_name, file).then(function (rs) {
+                    let ws = fs.createWriteStream(tmpDestination); // Open file descriptor
+                    rs.pipe(ws); //Pipe received data to be written in the file
+
+                    // Listen to then end of the file transfer
+                    resolve(new Promise(function (file_resolve, file_reject) {
+                        rs.on('end', function () {
+                            file_resolve(true);
+                        });
+                        rs.on('error', function (error) {
+                            file_reject(ErrorHelper('Downloading ' + file + ' failed', error));
+                        });
+                    }));
+                }, function (error) {
+                    reject(ErrorHelper('Downloading ' + file + ' has error', error));
+                });
+
+                // Push the file transfer promise into array
+                file_transfer_promises.push(p);
             });
 
-            // Push the file transfer promise into array
-            file_transfer_promises.push(p);
-        });
-
-        //Wait for all files to be transferred
-        Promise.all(file_transfer_promises).then(function(__unused__ok_array) {
-            //Create output directory if doesnt exists
-            if (fs.existsSync(path.join(_this._tmpDirectory, 'input', 'output')) === false) {
-                fs.mkdirSync(path.join(_this._tmpDirectory, 'input', 'output'));
-            }
-            resolve(true); // All good
-        }, function(error) {
-            reject(ErrorHelper('Input download failed', error));
-        });
+            //Wait for all files to be transferred
+            Promise.all(file_transfer_promises).then(function (__unused__ok_array) {
+                //Create output directory if doesnt exists
+                if (fs.existsSync(path.join(_this._tmpDirectory, 'input', 'output')) === false) {
+                    fs.mkdirSync(path.join(_this._tmpDirectory, 'input', 'output'));
+                }
+                resolve(true); // All good
+            }, function (error) {
+                reject(ErrorHelper('Input download failed', error));
+            });
+        }
     });
 };
 
