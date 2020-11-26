01195683d98a387fc26bd8d622da61bed8cb88d1
GuanyuTao <guanyu.tao@outlook.com>
2020-11-26 22:40:45 +0000

    add minio configurations and "storageComponent" to indicate minio or swift will be used.

--- packages/eae-carrier/config/eae.carrier.sample.config.js
+++ packages/eae-carrier/config/eae.carrier.sample.config.js
@@ -2,8 +2,15 @@
 module.exports = {
     mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
     port: 8080,
-	enableCors: true,
+    enableCors: true,
     swiftURL: 'http://0.0.0.0:8080',
     swiftUsername: 'root',
-    swiftPassword: 'root'
+    swiftPassword: 'root',
+    minIOHost: 'http://0.0.0.0',
+    minIOPort: 9000,
+    accessKey: 'minioadmin',
+    secretKey: 'minioadmin',
+    bucketRegion: 'region',
+    useSSL: false,
+    storageComponent: 'swift'
 };
