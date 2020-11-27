51d1a3801c11cd82a29bf697eaca56520b04539c
GuanyuTao <guanyu.tao@outlook.com>
2020-11-27 00:11:34 +0000

    fix some bugs

--- packages/eae-carrier/src/minIOStorage.js
+++ packages/eae-carrier/src/minIOStorage.js
@@ -24,7 +24,7 @@ function MinIOStorage(
     this._secretKey = options.secretKey;
     this._bucketRegion = options.bucketRegion;
     this._useSSL = options.useSSL;
-    this._bucketName = jobID + '_' + type;
+    this._bucketName = jobID + '-' + type;
 
     // Bind public member functions
     this.createObject = MinIOStorage.prototype.createObject.bind(this);
@@ -56,7 +56,8 @@ MinIOStorage.prototype._internalClosure = function (callback, ...args) {
                 useSSL: _this._useSSL,
                 accessKey: _this._accessKey,
                 secretKey: _this._secretKey
-            }).then(function (connected){
+            });
+            _this._client.listBuckets().then(function (connected){
                 resolve(connected);
             }, function (error){
                 reject(ErrorHelper('Failed to connect', error));
@@ -66,23 +67,27 @@ MinIOStorage.prototype._internalClosure = function (callback, ...args) {
 
     let _bucket_check = function () {
         return new Promise(function (resolve, reject) {
-            const bucketExists = _this._client.bucketExists(_this._bucketName);
-            if (!bucketExists) {
-                _this.client.makeBucket(_this._bucketName, _this._bucketRegion ?? '').then(function (create_ok){
-                    resolve(create_ok);
-                }, function (error){
-                    reject(ErrorHelper('Failed to create bucket', error));
-                });
-            }
+            _this._client.bucketExists(_this._bucketName).then(function (exists) {
+                if (!exists) {
+                    _this._client.makeBucket(_this._bucketName, _this._bucketRegion ?? '').then(function (create_ok){
+                        resolve(create_ok);
+                    }, function (error){
+                        reject(ErrorHelper('Failed to create bucket', error));
+                    });
+                }
+                else{
+                    resolve(true);
+                }
+            });
         });
     };
 
     return _connect_check().then(function (_unused___connect_ok) {
-            return _bucket_check().then(function (_unused__container_ok) {
-                return callback.apply(_this, args);
+        return _bucket_check().then(function (_unused__container_ok) {
+            return callback.apply(_this, args);
             }, function (bucket_error) {
-                return Promise.reject(ErrorHelper('Bucket check failed', bucket_error));
-            });
+            return Promise.reject(ErrorHelper('Bucket check failed', bucket_error));
+        });
         }, function (connect_error) {
             return Promise.reject(ErrorHelper('Connect check failed', connect_error));
         }
@@ -100,21 +105,16 @@ MinIOStorage.prototype.createObject = function (object_data, fileName) {
     let _this = this;
     return _this._internalClosure(function () {
         return new Promise(function (resolve, reject) {
+
             let objName = fileName || _this._generateFilename();
-            let fileExists;
-            try {
-                this._client.statObject(_this._bucketName, objName);
-                fileExists = true;
-            } catch (e) {
-                fileExists = false;
-            }
-            if (fileExists) {
-                throw new Error(`File "${fileName}" in bucket "${_this._bucketName}" already exists.`);
-            }
-            _this._client.putObject(_this._bucketName, objName, object_data).then(function(_unused__obj_ok){
-                resolve(objName);
-            }, function (obj_error){
-                reject(ErrorHelper('Creating new entry in object store failed', obj_error));
+            _this._client.statObject(_this._bucketName, objName).then(function (_unused__stat_ok){
+                throw new Error(`File "${objName}" in bucket "${_this._bucketName}" already exists.`);
+            }, function (_unused__error){
+                _this._client.putObject(_this._bucketName, objName, object_data).then(function(_unused__obj_ok){
+                    resolve(objName);
+                }, function (obj_error){
+                    reject(ErrorHelper('Creating new entry in object store failed', obj_error));
+                });
             });
         });
     });
